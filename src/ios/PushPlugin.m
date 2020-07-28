83a1c8a31a256eafa67f39bee46ef7cc3983555a
Bogdan Geleta <bogdan.geleta@gmail.com>
2020-07-28 14:38:08 +0300

    Update PushPlugin.m

--- src/ios/PushPlugin.m
+++ src/ios/PushPlugin.m
@@ -80,7 +80,6 @@
 #if !TARGET_IPHONE_SIMULATOR
     // A rotation of the registration tokens is happening, so the app needs to request a new token.
     NSLog(@"The FCM registration token needs to be changed.");
-    [[FIRInstanceID instanceID] token];
     [self initRegistration];
 #endif
 }
